c1162e0a6fdcee6d883fb1be92b2f3029f478518
RAAGUL VIGNESH <rv0815@srmist.edu.in>
2024-01-22 16:55:57 +0530

    [Raagul] ADD : ADDED A USECASE[UC1]

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Single Player Snake and Ladder Game!");
+        playGame();
+    }
+    private static void playGame(){
+        int playerpos=0;
+        System.out.println("Initial player position : "+playerpos);
+    }
+}
