0a9e0fdf81b9fb5c36e2808ee95b64820bf3ec92
RAAGUL VIGNESH <rv0815@srmist.edu.in>
2024-01-22 17:23:43 +0530

    [Raagul] UPDATE : UPDATED Usecase [UC2]

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Welcome to Single Player Snake and Ladder Game!");
         playGame();
     }
+    private static int rolldice(){
+        Random r = new Random();
+        return r.nextInt(6);
+    }
     private static void playGame(){
+        Scanner scanner = new Scanner(System.in);
         int playerpos=0;
         System.out.println("Initial player position : "+playerpos);
+        System.out.println("ENTER TO ROLL THE DIE :");
+        String stringValue = scanner.nextLine();
+        int dicevalue = rolldice();
+        System.out.println("You Rolled : "+dicevalue);
     }
 }
