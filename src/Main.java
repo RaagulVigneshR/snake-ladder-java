a68e9eafd727fe3ead8397aa6ba71f76f26a35b3
RAAGUL VIGNESH <rv0815@srmist.edu.in>
2024-01-22 17:52:23 +0530

    [Raagul] UPDATE : UPDATED Usecase [UC3]

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,36 @@ public class Main {
         return r.nextInt(6);
     }
     private static void playGame(){
+        int playerPosition = 0;
+        int targetPosition = 100;
         Scanner scanner = new Scanner(System.in);
-        int playerpos=0;
         System.out.println("Initial player position : "+playerpos);
         System.out.println("ENTER TO ROLL THE DIE :");
         String stringValue = scanner.nextLine();
         int dicevalue = rolldice();
         System.out.println("You Rolled : "+dicevalue);
     }
+    private static int checkSnakeAndLadder(int position) {
+        switch (position) {
+            case 12:
+                System.out.println("You landed on a ladder! Move to position 37.");
+                return 37;
+            case 21:
+                System.out.println("Oops! You landed on a snake. Move back to position 9.");
+                return 9;
+            case 28:
+                System.out.println("You landed on a ladder! Move to position 84.");
+                return  84;
+            case 51:
+                System.out.println("Oops! You landed on a snake. Move back to position 7.");
+                return 7;
+            case 71:
+                System.out.println("You landed on a ladder! Move to position 91.");
+                return 91;
+            case 87:
+                System.out.println("Oops! You landed on a snake. Move back to position 24.");
+                return 24;
+            default:
+                return position;
+        }
 }
